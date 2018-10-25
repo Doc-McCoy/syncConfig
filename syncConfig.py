6e7f47f6c89e141234de588044d1394cd60b3cd3
Doc McCoy <renan_ota@hotmail.com>
2018-10-25 18:25:10 -0300

    Bagunça transformada em classes.

--- syncConfig.py
+++ syncConfig.py
@@ -1,26 +1,31 @@
 import configparser
 from ftplib import FTP
 
-config = configparser.ConfigParser()
-config.read('params.ini')
-user = config['conn']['user']
-passwd = config['conn']['passwd']
+class SyncConfig:
 
-path_v1 = config['path_local']['v1_local']
-path_v2 = config['path_local']['v2_local']
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('params.ini')
+        self.user = config['conn']['user']
+        self.passwd = config['conn']['passwd']
+        self.path_v1 = config['path_local']['v1_local']
+        self.path_v2 = config['path_local']['v2_local']
+        self.remote_v1 = config['path_remoto']['v1_remoto']
+        self.remote_v2 = config['path_remoto']['v2_remoto']
 
-ftp_path_v1 = config['path_remoto']['v1_remoto']
-ftp_path_v2 = config['path_remoto']['v2_remoto']
+    def connectFTP(self):
+        self.ftp = FTP('svrdev001')
+        self.ftp.login(user=self.user, passwd=self.passwd)
 
-ftp = FTP('svrdev001')
-ftp.login(user=user, passwd=passwd)
+    def openFiles(self):
+        self.config_v1 = open(self.path_v1, 'rb')
+        self.config_v2 = open(self.path_v2, 'rb')
 
-config_v1 = open(path_v1, 'rb')
-config_v2 = open(path_v2, 'rb')
+    def saveFilesOnServer(self):
+        self.ftp.storbinary('STOR {}'.format(ftp_path_v1), self.config_v1)
+        self.ftp.storbinary('STOR {}'.format(ftp_path_v2), self.config_v2)
 
-ftp.storbinary('STOR {}'.format(ftp_path_v1), config_v1)
-ftp.storbinary('STOR {}'.format(ftp_path_v2), config_v2)
-
-config_v1.close()
-config_v2.close()
-ftp.quit()
+    def __destroy__(self):
+        self.config_v1.close()
+        self.config_v2.close()
+        self.ftp.quit()
