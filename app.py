d0ef769ff2b257d84ca2c23f4b51aabb9be0d726
Doc McCoy <renan_ota@hotmail.com>
2018-10-26 00:30:03 -0300

    Upload da GUI e funções da classe sync.

---
+++ app.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from syncConfig import SyncConfig
+
+class App():
+
+    def __init__(self, tk):
+        self.sync = SyncConfig()
+        tk.title("INI Editor")
+        root = Frame(tk)
+        root.pack()
+        # Labels
+        Label(root, text="v1").grid(row=0, column=0)
+        Label(root, text="v2").grid(row=0, column=1)
+        # Textareas
+        self.textArea1 = Text(root)
+        self.textArea2 = Text(root)
+        self.textArea1.grid(row=1, column=0)
+        self.textArea2.grid(row=1, column=1)
+        filesContent = self.sync.getFilesContent()
+        self.textArea1.insert(INSERT, filesContent[0])
+        self.textArea2.insert(INSERT, filesContent[1])
+        # Botões
+        frameBtn = Frame(root).grid(row=2, column=0, columnspan=2)
+        Button(frameBtn, text="Salvar", command=self.saveFile).pack(side=RIGHT)
+        Button(frameBtn, text="Sincronizar", command=self.sync.syncFiles).pack(side=RIGHT)
+
+    def saveFile(self):
+        content1 = self.textArea1.get("1.0", END)
+        content2 = self.textArea2.get("1.0", END)
+        self.sync.saveFilesContent(content1, content2)
+
+if __name__ == '__main__':
+    tk = Tk()
+    app = App(tk)
+    tk.mainloop()
--- syncConfig.py
+++ syncConfig.py
@@ -1,4 +1,4 @@
-import configparser
+import configparser, os.path
 from ftplib import FTP
 
 class SyncConfig:
@@ -17,15 +17,41 @@ class SyncConfig:
         self.ftp = FTP('svrdev001')
         self.ftp.login(user=self.user, passwd=self.passwd)
 
-    def openFiles(self):
+    def readFilesBinary(self):
         self.config_v1 = open(self.path_v1, 'rb')
         self.config_v2 = open(self.path_v2, 'rb')
 
     def saveFilesOnServer(self):
-        self.ftp.storbinary('STOR {}'.format(ftp_path_v1), self.config_v1)
-        self.ftp.storbinary('STOR {}'.format(ftp_path_v2), self.config_v2)
+        self.ftp.storbinary('STOR {}'.format(self.remote_v1), self.config_v1)
+        self.ftp.storbinary('STOR {}'.format(self.remote_v2), self.config_v2)
 
-    def __destroy__(self):
+    def syncFiles(self):
+        self.connectFTP()
+        self.readFilesBinary()
+        self.saveFilesOnServer()
+        self.ftp.quit()
         self.config_v1.close()
         self.config_v2.close()
-        self.ftp.quit()
+
+    def getFilesContent(self):
+        if os.path.isfile(self.path_v1):
+            v1 = open(self.path_v1, 'r')
+            conteudo_v1 = v1.read()
+            v1.close()
+        else:
+            conteudo_v1 = 'Config v1 não encontrado.'
+        if os.path.isfile(self.path_v2):
+            v2 = open(self.path_v2, 'r')
+            conteudo_v2 = v2.read()
+            v2.close()
+        else:
+            conteudo_v2 = "Config v2 não encontrado."
+        return [conteudo_v1, conteudo_v2]
+
+    def saveFilesContent(self, content1, content2):
+        file1 = open(self.path_v1, 'w')
+        file2 = open(self.path_v2, 'w')
+        file1.write(content1)
+        file2.write(content2)
+        file1.close()
+        file2.close()
