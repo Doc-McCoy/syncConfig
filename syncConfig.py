6a8e77bc5d929164fb99db4fb50817fefb884273
Doc McCoy <renan_ota@hotmail.com>
2018-11-01 00:23:35 -0300

    Commit parcial

--- syncConfig.py
+++ syncConfig.py
@@ -6,10 +6,10 @@ class SyncConfig:
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config.read('params.ini')
-        self.user = config['conn']['user']
-        self.passwd = config['conn']['passwd']
-        self.paths_locais = config.options('path_local')
-        self.paths_remotos = config.options('path_remoto')
+        self.user = self.config['conn']['user']
+        self.passwd = self.config['conn']['passwd']
+        self.paths_locais = self.config.options('local')
+        self.paths_remotos = self.config.options('remoto')
         # path_v1
         # path_v2
 
@@ -20,14 +20,14 @@ class SyncConfig:
     def readFilesBinary(self): # MUDAR ESSE
         self.files = []
         for path in self.paths_locais:
-            self.files.append(open(self.config[path]))
+            self.files.append(open(self.config['local'][path]))
 
     def saveFilesOnServer(self):
         self.ftp.storbinary('STOR {}'.format(self.remote_v1), self.config_v1)
         self.ftp.storbinary('STOR {}'.format(self.remote_v2), self.config_v2)
 
     def syncFiles(self):
-        self.connectFTP()y
+        self.connectFTP()
         self.readFilesBinary()
         self.saveFilesOnServer()
         self.ftp.quit()
@@ -37,8 +37,8 @@ class SyncConfig:
     def getFilesContent(self): # MUDAR ESSE
         self.files_content = []
         for path in self.paths_locais:
-            if os.path.isfile(self.config[path]):
-                file = open(self.config[path], 'r', encoding='ISO-8859-1')
+            if os.path.isfile(self.config['local'][path]):
+                file = open(self.config['local'][path], 'r', encoding='ISO-8859-1')
                 content = file.read()
                 self.files_content.append(content)
                 file.close()
@@ -49,6 +49,10 @@ class SyncConfig:
 
     def saveFilesContent(self, contents): # MUDAR ESSE
         for path in self.paths_locais:
-            file = open(self.config[path], 'w', encoding='ISO-8859-1')
+            file = open(self.config['local'][path], 'w', encoding='ISO-8859-1')
             file.write(contents[0]) # ARRUMAR ISSO
             file.close()
+
+teste = SyncConfig()
+
+teste.getFilesContent()
