d2d8a132165706d842602fa4d7b72a91092709ed
Doc McCoy <renan_ota@hotmail.com>
2018-11-06 01:11:39 -0200

    Commit parcial

--- app.py
+++ app.py
@@ -9,19 +9,23 @@ class App():
         root = Frame(tk)
         root.pack()
         # Labels
-        Label(root, text="v1").grid(row=0, column=0)
-        Label(root, text="v2").grid(row=0, column=1)
+        # Label(root, text="v1").grid(row=0, column=0)
+        # Label(root, text="v2").grid(row=0, column=1)
         # Textareas
-        # Aqui vai um loop que cria os texts conforme existam no config
-        self.textArea1 = Text(root)
-        self.textArea2 = Text(root)
-        self.textArea1.grid(row=1, column=0)
-        self.textArea2.grid(row=1, column=1)
         filesContent = self.sync.getFilesContent()
-        self.textArea1.insert(INSERT, filesContent[0])
-        self.textArea2.insert(INSERT, filesContent[1])
+        self.textareas = []
+        frameTextArea = Frame(tk)
+        frameTextArea.pack()
+
+        for file in filesContent:
+            textArea = Text(frameTextArea)
+            textArea.pack(side=LEFT)
+            textArea.insert(INSERT, file)
+            self.textareas.append(file)
+
         # Botões
-        frameBtn = Frame(root).grid(row=2, column=0, columnspan=2)
+        frameBtn = Frame(tk)
+        frameBtn.pack(fill='x')
         Button(frameBtn, text="Salvar", command=self.saveFile).pack(side=RIGHT)
         Button(frameBtn, text="Sincronizar", command=self.sync.syncFiles).pack(side=RIGHT)
 
